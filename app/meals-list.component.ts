505beaac1e7900534da8b512b4a3cc64074aeb51
Angela Gathu <agathu87@gmail.com>
2017-09-20 11:26:50 +0300

    update current file

---
+++ app/meals-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'meals-list',
+  template: `
+
+  `
+})
+
+export class MealsListComponent{
+  @Input() childMealsList: Meal[];
+  @Output() clickSender = new EventEmitter();
+  editButtonHasBeenClicked(mealToEdit: Meal){
+    console.log(mealToEdit);
+    this.clickSender.emit(mealToEdit);
+  }
+}
