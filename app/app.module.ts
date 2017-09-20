ac6313a94b3902216a8d31509b1e8596cda81fa3
Angela Gathu <agathu87@gmail.com>
2017-09-20 12:09:23 +0300

    include other imports

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,7 @@
 import { NgModule } from '@angular/core';
 import { EditMealComponent } from './edit-meal.component';
 import { AppComponent } from './app.component';
+import { MealsListComponent } from './meals-list.component';
+import { NewMealComponent } from './new-meal.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
